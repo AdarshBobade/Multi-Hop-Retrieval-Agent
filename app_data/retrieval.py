fa6bce379ee3be33816d18e04f01c8dbe60c8c09
AdarshBobade <adarsh.bobade26@gmail.com>
2026-08-04 17:38:59 +0530

    Changed params

--- app_data/retrieval.py
+++ app_data/retrieval.py
@@ -1,6 +1,6 @@
 from app_data.ingestion import collection
 
-def retrieve(query , k_closest = 3):
+def retrieve(query , k_closest = 5):
     results = collection.query(query_texts=[query] , n_results=k_closest)
     return results['documents'][0]
 
