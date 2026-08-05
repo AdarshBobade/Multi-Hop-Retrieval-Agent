b1dd4080b31f83cb86949e43b9e07a878ed1c4a3
AdarshBobade <adarsh.bobade26@gmail.com>
2026-08-06 00:11:24 +0530

    Breaks the user query into subquestions using PLANNER_SYSTEM_PROMPT and returns JSON

---
+++ app_data/decomposition.py
@@ -0,0 +1,42 @@
+from groq import Groq
+from dotenv import load_dotenv
+from app_data.config import groq_api
+from app_data.prompts import PLANNER_SYSTEM_PROMPT
+import json
+load_dotenv()
+
+client = Groq(api_key=groq_api)
+
+# Breaking the user query into sub_questions
+def planner(query:str):
+
+ 
+    response = client.chat.completions.create(
+                    model="llama-3.3-70b-versatile",
+                    messages=[
+                        {"role": "system", "content": PLANNER_SYSTEM_PROMPT},
+                        {"role": "user" , "content" : query}]
+                    )
+
+    # LLM Returns a string containing JSON :
+    # SO first Parsing JSON :
+    
+    
+    try :
+        research_plan = json.loads(response.choices[0].message.content)
+        return research_plan
+    except json.JSONDecodeError :
+        return {
+                    "complexity": "simple",
+                    "goal": query,
+                    "sub_questions": [
+                        {
+                            "question": query,
+                            "purpose": "Fallback to original query due to planner parsing failure."
+                        }
+                    ],
+                    "planner_status": "fallback"
+                }
+    
+
+
